a3b13c78f144610d8bde87ecc14fe6f9e260b0b1
Dakshayani <vaishnavdakshayani16@gmail.com>
2025-02-22 01:29:19 +0530

    Added backend files

---
+++ config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+'mongodb+srv://vaishnavdakshayani16:123@cluster0.mb7rn.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Cluster0'
---
+++ index.js
@@ -0,0 +1,47 @@
+import express, { request, response } from 'express';
+import {Book} from "./models/bookModel.js"
+import { PORT,mongoDBURL } from "./config.js"
+import mongoose from 'mongoose';
+import booksRoute from "./routes/booksRoute.js"
+import cors from 'cors';
+const app = express();
+
+app.use(
+    cors({
+      origin: 'http://localhost:5173',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type'],
+    })
+  );
+  
+  app.use(express.json());
+// middleware for parsing request body
+app.use(express.json());
+app.get('/',(request,response) =>{
+    console.log(request)
+    return response.status(234).send("Welcome to  Book store Project")
+});
+app.use('/books',booksRoute);
+
+// CORS (Cross-Origin Resource Sharing) is a security feature in web browsers 
+// that controls how resources on a web page can be requested from 
+// a different origin (domain, protocol, or port). It prevents 
+// unauthorized cross-origin requests but allows controlled access 
+// through HTTP headers
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+// app.use(cors());
+// Option 2: Allow Custom Origins
+
+mongoose
+.connect(mongoDBURL)  // 1️⃣ Connect to MongoDB using the provided URL
+.then(() => {         // 2️⃣ If the connection is successful, execute this block
+    console.log("Connected to MongoDB successfully!");
+    app.listen(PORT,() =>{
+        console.log(`App is listening to port: ${PORT}`);
+    });
+})
+.catch((error) => {   // 3️⃣ If the connection fails, handle the error
+    console.error("Error connecting to MongoDB:", error);
+});
